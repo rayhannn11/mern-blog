42cd8df651a1e6c73a2cfbd94f3e0917f4f3738a
rayhannn11 <rayhan.onepiece@gmail.com>
2024-09-18 18:31:22 +0700

    fix proxy issue

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "https://mern-blog-blush-three.vercel.app",
-        secure: false,
+        secure: true,
       },
     },
   },
