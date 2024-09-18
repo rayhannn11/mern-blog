92fbf5584fbe298324885d5a9ce8ec27ce51e3c2
rayhannn11 <rayhan.onepiece@gmail.com>
2024-09-18 18:05:08 +0700

    change url domain

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
+      "/api": {
+        target: "https://mern-blog-blush-three.vercel.app",
         secure: false,
       },
     },
